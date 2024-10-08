d874011c1393721a4c6240b2d7a5c2f4c035000a
eddydevfe <pedro@null.>
2024-10-08 11:41:09 -0300

    Update imports in LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,17 +1,10 @@
-import Navbar from "../components/Navbar/Navbar.tsx";
-import Hero from "../components/Hero/Hero.tsx";
-import ImageCarousel from "../components/ImageCarousel/ImageCarousel.tsx";
-import WhyChooseUs from "../components/WhyChooseUs/WhyChooseUs.tsx";
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/Hero";
+import ImageCarousel from "../components/ImageCarousel/ImageCarousel";
+import WhyChooseUs from "../components/WhyChooseUs/WhyChooseUs";
 import Quotes from "../components/Quotes/Quotes.tsx";
-import FooterEngage from "../components/FooterEngage/FooterEngage.tsx";
-import Footer from "../components/Footer/Footer.tsx";
-
-/*
-    That FooterEngage used to be inside the footer but I had to remove
-    because I could not use the proper CSS for the slight gradient in 
-    the whole footer and I could not do it to a single section because
-    of the container.
-*/
+import FooterEngage from "../components/FooterEngage/FooterEngage";
+import Footer from "../components/Footer/Footer";
 
 const LandingPage = () => {
   return (
